ba51db655dc2249eae44735cef4a9aaf2e908b6a
m.oynoshev <m.oynoshev@tinkoff.ru>
2023-06-25 13:05:44 +0300

    Add fetching topics from optimus prime

--- src/components/TopicList/TopicList.tsx
+++ src/components/TopicList/TopicList.tsx
@@ -1,11 +1,12 @@
-import Topic from '../Topic/Topic';
-import classes from './TopicList.module.css';
+import Topic from "../Topic/Topic";
+import { TopicEntity } from "../../domain/topics.entities";
+import classes from "./TopicList.module.css";
 
-function TopicList({ topics }: { topics: string[] }) {
+function TopicList({ topics }: { topics: TopicEntity[] }) {
   return (
     <div className={classes.TopicList}>
       {topics.map((topic, i) => (
-        <Topic text={topic} key={i} />
+        <Topic text={topic.name} key={i} />
       ))}
     </div>
   );
